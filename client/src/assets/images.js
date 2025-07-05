b2e54241ea67d4306813b1070c655ec8e9757a26
Nebiyu Daniel <danielnebiyuneb@gmail.com>
2025-07-05 18:05:18 +0300

    Assets

--- client/src/assets/images.js
+++ client/src/assets/images.js
@@ -5,6 +5,17 @@ import MachineLearning from '../assets/MachineLearning.jpg'
 import Relativity from '../assets/Astrophycis.jpg'
 import Quantummechanics from '../assets/Quantummechanics.jpg'
 import SciComp from '../assets/SciComp.jpg'
+import Blackholesimulation from '../assets/Blackhole_simmulation.jpg'
+import NeuralNetworks from '../assets/Neural_Networks.jpg'
+import Quantumcom from '../assets/Quantumcom.jpg'
+import Feymanpath from '../assets/Feymanpath.jpg'
+import Gpuacceleration from '../assets/Gpuacceleration.jpg'
+import Standardmodelinpython from '../assets/Standardmodelinpython.jpg'
+import QuantumandML from '../assets/QuantumandML.jpg'
+import Nebiyu from '../assets/Nebiyu.jpg'
+import Networks from '../assets/Neuralnetwork.jpg'
+import PyCode from '../assets/PyCode.jpg'
+import QunEntanglement from '../assets/QunEntanglement.jpg'
 
 export const images = {
     Heroimage,
@@ -13,5 +24,16 @@ export const images = {
     MachineLearning,
     Relativity,
     Quantummechanics,
-    SciComp
+    SciComp,
+    Blackholesimulation,
+    NeuralNetworks,
+    Quantumcom,
+    Feymanpath,
+    Gpuacceleration,
+    Standardmodelinpython,
+    QuantumandML,
+    Nebiyu,
+    Networks,
+    PyCode,
+    QunEntanglement
 }
