b835058562fd40eae54b3d2417b9206a88e50e81
Nebiyu Daniel <danielnebiyuneb@gmail.com>
2025-07-09 12:47:23 +0300

    App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,39 +1,30 @@
-import React from "react";
-import Nav from "./components/Nav";
-import Hero from "./components/Hero";
-import Categories from "./components/Categories";
-import FeaturedArticles from "./components/FeaturedArticles";
-import LatestArticles from "./components/LatestArticles";
-import Proflie from "./components/Proflie";
-import Topics from "./components/Topics";
-import Populartages from "./components/Populartages";
-import Subscribe from "./components/Subscribe";
-import RecentPost from "./components/RecentPost";
-import Footer from "./components/Footer";
-
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Blog from './pages/Blog'
+import Articles from './pages/Articles'
+import Layout from './pages/Admin/Layout'
+import Dashboard from './pages/Admin/Dashboard'
+import AddBlog from './pages/Admin/AddBlog'
+import BlogList from './pages/Admin/BlogList'
+import Comments from './pages/Admin/Comments'
+import Login from './components/Admin/Login'
 function App() {
   return (
     <div>
-      <Nav />
-      <div className="container mx-auto px-4 py-8 flex flex-col md:flex-row">
-        <div className="w-full md:w-3/4 md:pr-8">
-          <Hero />
-          <Categories />
-          <FeaturedArticles />
-          <LatestArticles />
-        </div>
-
-        <div className="w-full md:w-1/4 mt-8 md:mt-0">
-          <Proflie />
-          <Topics/>
-          <Populartages/>
-          <Subscribe/>
-          <RecentPost/>
-        </div>
-      </div>
-      <Footer/>
+      <Routes>
+        <Route path='/'element={<Home/>}/>
+        <Route path='/articles'element={<Articles/>}/>
+        <Route path='/blog/:id'element={<Blog/>}/>
+        <Route path='/admin' element={true ?  <Layout/> : <Login/>}>
+        <Route index element={<Dashboard/>}/>
+        <Route path='addBlog' element={<AddBlog/>}/>
+        <Route path='listBlog' element={<BlogList/>}/>
+        <Route path='comments' element={<Comments/>}/>
+        </Route>
+      </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
