7a390887f020c861032a0570e6b278774232524c
Nebiyu Daniel <danielnebiyuneb@gmail.com>
2025-07-09 12:42:27 +0300

    Articles

---
+++ client/src/pages/Articles.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useAppContext } from '../Context/AppContext'
+
+function Articles() {
+  const {navigate} = useAppContext();
+  return (
+    <div>
+      <h1 className='text-5xl'>This is article page.</h1>
+    </div>
+  )
+}
+
+export default Articles
