24bc54a459dd6efeada3062cdf40ab3cb88681a3
Nebiyu Daniel <danielnebiyuneb@gmail.com>
2025-07-10 14:26:30 +0300

    Packages

--- client/src/pages/Admin/AddBlog.jsx
+++ client/src/pages/Admin/AddBlog.jsx
@@ -1,11 +1,113 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from "react";
+import {
+  assets,
+  blogCategories,
+} from "../../assets/QuickBlog-Assets/assets.js";
+import Quill from "quill";
 
 function AddBlog() {
+  const editorRef = useRef(null);
+  const quillRef = useRef(null);
+  const [image, setImage] = useState(false);
+  const [title, setTitle] = useState("");
+  const [subTitle, setSubTitle] = useState("");
+  const [category, setCategory] = useState("Startup");
+  const [isPublished, setIsPublished] = useState(false);
+
+  const onSubmitHandeler = async (e) => {
+    e.preventDefault();
+  };
+
+  const generateContent = async () => {};
+
+  useEffect(() => {
+    if (!quillRef.current && editorRef.current) {
+      quillRef.current = new Quill(editorRef.current, { theme: "snow" });
+    }
+  }, []);
+
   return (
-    <div>
-        
-    </div>
-  )
+    <form
+      onSubmit={onSubmitHandeler}
+      className="flex-1 bg-blue-50/50 text-gray-600 h-full overflow-scroll"
+    >
+      <div className="p-4 md:p-10 sm:m-10 max-w-3xl w-full bg-white shadow rounded">
+        <p>Upload thumbnail</p>
+        <label htmlFor="image">
+          <img
+            src={!image ? assets.upload_area : URL.createObjectURL(image)}
+            alt="Upload image"
+            className="mt-2 h-16 rounded cursor-pointer"
+          />
+          <input
+            onChange={(e) => setImage(e.target.files[0])}
+            type="file"
+            id="image"
+            hidden
+            required
+          />
+        </label>
+
+        <p className="mt-4">Blog title</p>
+        <input
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          type="text"
+          placeholder="What is your title?"
+          required
+          className="w-full max-w-lg mt-2 p-2 border border-gray-300 outline-none rounded"
+        />
+        <p className="mt-4">Blog Subtitle</p>
+        <input
+          value={subTitle}
+          onChange={(e) => setSubTitle(e.target.value)}
+          type="text"
+          placeholder="And subtitle, please? "
+          required
+          className="w-full max-w-lg mt-2 p-2 border border-gray-300 outline-none rounded"
+        />
+        <p className="mt-4">Blog Discription</p>
+        <div className="max-w-lg h-74 pb-16 sm:pb-10 pt-2 relative">
+          <div ref={editorRef}></div>
+          <button
+            onClick={generateContent}
+            type="button"
+            className="absolute bottom-1 right-2 ml-2 text-xs text-white bg-black/70 px-4 py-1.5 rounded hover:underline cursor-pointer"
+          >
+            Generate with AI
+          </button>
+        </div>
+        <p className="mt-4">Blog Category</p>
+        <select
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+          name="category"
+          className="mt-2 px-3 py-2 border text-gray-500 border-gray-300 outline-none rounded"
+        >
+          <option value="">Select category</option>
+          {blogCategories.map((category, index) => {
+            return (
+              <option key={index} value={category}>
+                {category}
+              </option>
+            );
+          })}
+        </select>
+        <div className="flex gap-2 mt-4">
+          <p>Publish Now</p>
+          <input
+            checked={isPublished}
+            onChange={(e) => setIsPublished(e.target.checked)}
+            type="checkbox"
+            className="scale-125 cursor-pointer"
+          />
+        </div> 
+        <button type="submit" className="mt-8 w-40 h-10 bg-primary text-white rounded cursor-pointer text-sm">
+          Go Post!
+        </button>
+      </div>
+    </form>
+  );
 }
 
-export default AddBlog
+export default AddBlog;
