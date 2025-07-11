e840e69e2182e961aeac4dcdb7d1c258300e2d49
Nebiyu Daniel <danielnebiyuneb@gmail.com>
2025-07-11 09:11:59 +0300

    Admin Routes

--- server/Routes/adminRoutes.js
+++ server/Routes/adminRoutes.js
@@ -1,8 +1,21 @@
-import express from 'express'
-import { adminLogin } from '../Controllers/adminController.js';
+import express from "express";
+import {
+  adminLogin,
+  approveCommentByID,
+  deleteCommentById,
+  getAllBlogsAdmin,
+  getAllComments,
+  getDashBoard,
+} from "../Controllers/adminController.js";
+import auth from "../Middlewares/auth.js";
 
 const adminRouter = express.Router();
 
-adminRouter.post('/login', adminLogin)
+adminRouter.post("/login", adminLogin);
+adminRouter.get("/comments", auth, getAllComments);
+adminRouter.get("/blogs", auth, getAllBlogsAdmin);
+adminRouter.post("/delete-comment", auth, deleteCommentById);
+adminRouter.post("/approve-comment", auth, approveCommentByID);
+adminRouter.get("/dashboard", auth, getDashBoard);
 
-export default adminRouter
+export default adminRouter;
