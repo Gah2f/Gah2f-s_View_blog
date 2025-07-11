93c6d461841ede1c68592be59aac20c260583f72
Nebiyu Daniel <danielnebiyuneb@gmail.com>
2025-07-11 09:12:31 +0300

    Blod Routes

--- server/Routes/blogRoutes.js
+++ server/Routes/blogRoutes.js
@@ -1,10 +1,24 @@
 import express from "express";
-import { addBlog } from "../Controllers/blogController.js";
+import {
+  addBlog,
+  addComment,
+  deleteBlogId,
+  getAllBlogs,
+  getBlogById,
+  getBlogComments,
+  togglePublish,
+} from "../Controllers/blogController.js";
 import upload from "../Middlewares/multer.js";
 import auth from "../Middlewares/auth.js";
 
 const blogRouter = express.Router();
 
 blogRouter.post("/add", upload.single("image"), auth, addBlog);
+blogRouter.get("/all", getAllBlogs);
+blogRouter.get("/:blogId", getBlogById);
+blogRouter.post("/delete", auth, deleteBlogId);
+blogRouter.post("toggle-publish", auth, togglePublish);
+blogRouter.post('/add-comment', addComment);
+blogRouter.get('comments', getBlogComments)
 
 export default blogRouter;
