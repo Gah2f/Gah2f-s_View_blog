3cdbb2dad0d1b6f09f842338705d2fda33d0a809
Nebiyu Daniel <danielnebiyuneb@gmail.com>
2025-07-23 12:54:10 +0300

    Comment table items

--- client/src/components/Admin/CommentTableItems.jsx
+++ client/src/components/Admin/CommentTableItems.jsx
@@ -1,9 +1,45 @@
 import React from "react";
 import { assets } from "../../assets/QuickBlog-Assets/assets";
+import { useAppContext } from "../../Context/AppContext.jsx";
+import toast from "react-hot-toast";
 
 function CommentTableItems({ comment, fetchComments }) {
+  const { axios } = useAppContext();
   const { blog, createdAt, _id } = comment;
   const BlogDate = new Date(createdAt);
+
+  const approveComment = async () => {
+    try {
+      const { data } = await axios.post("/api/admin/approve-comment", { id: _id});
+      if (data.success) {
+        toast.success("Comment approved successfully");
+        await fetchComments();
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.log("Error approving comment:", error);
+      toast.error("Failed to approve comment");
+    }
+  } 
+
+  const deleteComment = async () => {
+    try { 
+
+      const confirm = window.confirm("Are you sure you want to delete this comment?");
+      if (!confirm) return;
+      const { data } = await axios.delete("/api/admin/delete-comment", { id: _id });
+      if (data.success) {
+        toast.success("Comment deleted successfully");
+        await fetchComments();
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.log("Error deleting comment:", error);
+      toast.error("Failed to delete comment");
+    }
+  }
   return (
     <tr className="border-y border-gray-300">
       <td className="px-6 py-4">
@@ -22,9 +58,9 @@ function CommentTableItems({ comment, fetchComments }) {
       <td className="px-6 py-4">
         <div className="inline-flex items-center gap-4">
             {!comment.isApproved ? 
-        <img src={assets.tick_icon} alt="Tick icon" className="w-5 hover:scale-110 transition-all cursor-pointer"/>    
+        <img onClick={approveComment} src={assets.tick_icon} alt="Tick icon" className="w-5 hover:scale-110 transition-all cursor-pointer"/>    
         : <p className="text-xs border border-green-600 bg-green-100 text-green-600 rounded-full px-3 py-1"> Approved </p>}
-        <img src={assets.bin_icon} alt="Bin icon" className="w-5 hover:scale-110 transition-all cursor-pointer"
+        <img onClick={deleteComment} src={assets.bin_icon} alt="Bin icon" className="w-5 hover:scale-110 transition-all cursor-pointer"
         />
         </div>
       </td>
