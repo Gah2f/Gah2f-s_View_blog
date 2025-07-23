6ffb0e5aa8e5ad6932fe30416dd6b6c85b59078b
Nebiyu Daniel <danielnebiyuneb@gmail.com>
2025-07-23 12:56:53 +0300

    Blog List

--- client/src/pages/Admin/BlogList.jsx
+++ client/src/pages/Admin/BlogList.jsx
@@ -1,57 +1,71 @@
-import React, { useEffect, useState } from 'react'
-import { blog_data } from '../../assets/QuickBlog-Assets/assets';
-import BlogTableItems from '../../components/Admin/BlogTableItems';
+import React, { useEffect, useState } from "react";
+import { blog_data } from "../../assets/QuickBlog-Assets/assets";
+import BlogTableItems from "../../components/Admin/BlogTableItems";
+import { useAppContext } from "../../Context/AppContext";
+import toast from "react-hot-toast";
 
-function BlogList() { 
-    const [blogs, setBlogs] = useState([]);
-    const fetchBlogs = async ()=>{
-        setBlogs(blog_data)
+function BlogList() {
+  const { axios } = useAppContext();
+  const [blogs, setBlogs] = useState([]);
+
+  const fetchBlogs = async () => {
+    try {
+      const { data } = await axios.get("/api/admin/blogs");
+
+      if (data.success) {
+        setBlogs(data.blogs);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.log("Error fetching blogs:", error);
+      toast.error("Failed to fetch blogs");
     }
-    useEffect(()=>{
-        fetchBlogs()
-    },[])
+  };
+  useEffect(() => {
+    fetchBlogs();
+  }, []);
+
   return (
-    <div className='flex-1 pt-5 px-5 sm:pt-12 sm:pl-16 bg-blue-50/50'>
-        <h1>
-            All Blogs
-        </h1>
+    <div className="flex-1 pt-5 px-5 sm:pt-12 sm:pl-16 bg-blue-50/50">
+      <h1>All Blogs</h1>
 
-        <div className="relative h-4/5 mt-4 max-w-4xl overflow-x-auto shadow rounded-lg scrollbar-hide bg-white">
-          <table className="w-full text-sm text-gray-500">
-            <thead className="text-xs text-gray-600 text-left uppercase">
-              <tr>
-                <th scope="col" className="px-2 py-4 xl:px-6">
-                  #
-                </th>
-                <th scope="col" className="px-2 py-4">
-                  Blog Title
-                </th>
-                <th scope="col" className="px-2 py-4 max-sm:hidden">
-                  Date
-                </th>
-                <th scope="col" className="px-2 py-4 max-sm:hidden">
-                  Status
-                </th>
-                <th scope="col" className="px-2 py-4">
-                  Action
-                </th>
-              </tr>
-            </thead>
+      <div className="relative h-4/5 mt-4 max-w-4xl overflow-x-auto shadow rounded-lg scrollbar-hide bg-white">
+        <table className="w-full text-sm text-gray-500">
+          <thead className="text-xs text-gray-600 text-left uppercase">
+            <tr>
+              <th scope="col" className="px-2 py-4 xl:px-6">
+                #
+              </th>
+              <th scope="col" className="px-2 py-4">
+                Blog Title
+              </th>
+              <th scope="col" className="px-2 py-4 max-sm:hidden">
+                Date
+              </th>
+              <th scope="col" className="px-2 py-4 max-sm:hidden">
+                Status
+              </th>
+              <th scope="col" className="px-2 py-4">
+                Action
+              </th>
+            </tr>
+          </thead>
 
-            <tbody>
-              {blogs.map((blog, index) => (
-                <BlogTableItems
-                  key={blog_data._id}
-                  blog={blog}
-                  fetchBlogs={fetchBlogs}
-                  index={index + 1}
-                />
-              ))}
-            </tbody>
-          </table>
-        </div>
-    </div> 
-  )
+          <tbody>
+            {blogs.map((blog, index) => (
+              <BlogTableItems
+                key={blog_data._id}
+                blog={blog}
+                fetchBlogs={fetchBlogs}
+                index={index + 1}
+              />
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
-export default BlogList
+export default BlogList;
