5dd272a2cd2fe0f240254272a341254e0afaeb7a
Nebiyu Daniel <danielnebiyuneb@gmail.com>
2025-07-23 12:56:12 +0300

    Add blog Page

--- client/src/pages/Admin/AddBlog.jsx
+++ client/src/pages/Admin/AddBlog.jsx
@@ -4,6 +4,9 @@ import {
   blogCategories,
 } from "../../assets/QuickBlog-Assets/assets.js";
 import Quill from "quill";
+import { useAppContext } from "../../Context/AppContext.jsx";
+import toast from "react-hot-toast";
+import { parse } from "marked";
 
 function AddBlog() {
   const editorRef = useRef(null);
@@ -13,12 +16,68 @@ function AddBlog() {
   const [subTitle, setSubTitle] = useState("");
   const [category, setCategory] = useState("Startup");
   const [isPublished, setIsPublished] = useState(false);
+  const [isAdding, setIsAdding] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { axios } = useAppContext();
 
   const onSubmitHandeler = async (e) => {
-    e.preventDefault();
+    try {
+      e.preventDefault();
+      setIsAdding(true);
+      const blog = {
+        title,
+        subTitle,
+        description: quillRef.current.root.innerHTML,
+        category,
+        isPublished,
+      };
+
+      const formData = new FormData();
+      formData.append("blog", JSON.stringify(blog));
+      formData.append("image", image);
+
+      const { data } = await axios.post("/api/blog/add", formData);
+
+      if (data.success) {
+        toast.success("Blog added successfully");
+        setImage(false);
+        setTitle("");
+        setSubTitle("");
+        setCategory("Physics Articles");
+        setIsPublished(false);
+        quillRef.current.root.innerHTML = "";
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.log("Error adding blog:", error);
+      toast.error("Failed to add blog");
+    } finally {
+      setIsAdding(false);
+    }
   };
 
-  const generateContent = async () => {};
+  const generateContent = async () => {
+    if (!title) return toast.error("Please enter a title");
+
+    try {
+      setIsLoading(true);
+      const { data } = await axios.post("/api/blog/generate", {
+        prompt: title,
+      });
+      if (data.success) {
+        quillRef.current.root.innerHTML = parse(data.content);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.log("Error generating content:", error);
+      toast.error("Failed to generate content");
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   useEffect(() => {
     if (!quillRef.current && editorRef.current) {
@@ -69,12 +128,18 @@ function AddBlog() {
         <p className="mt-4">Blog Discription</p>
         <div className="max-w-lg h-74 pb-16 sm:pb-10 pt-2 relative">
           <div ref={editorRef}></div>
+          {isLoading && (
+            <div className="absolute right-0 top-0 bottom-0 left-0 flex items-center justify-center bg-black/10 mt-2">
+              <div className="w-8 h-8 rounded-full border-2 border-t-white animate-spin "></div>
+            </div>
+          )}
           <button
+            disabled={isLoading}
             onClick={generateContent}
             type="button"
             className="absolute bottom-1 right-2 ml-2 text-xs text-white bg-black/70 px-4 py-1.5 rounded hover:underline cursor-pointer"
           >
-            Generate with AI
+            {isLoading ? "Generating..." : "Generate with AI"}
           </button>
         </div>
         <p className="mt-4">Blog Category</p>
@@ -101,9 +166,13 @@ function AddBlog() {
             type="checkbox"
             className="scale-125 cursor-pointer"
           />
-        </div> 
-        <button type="submit" className="mt-8 w-40 h-10 bg-primary text-white rounded cursor-pointer text-sm">
-          Go Post!
+        </div>
+        <button
+          disabled={isAdding}
+          type="submit"
+          className="mt-8 w-40 h-10 bg-primary text-white rounded cursor-pointer text-sm"
+        >
+          {isAdding ? "Adding..." : "Go Post!"}
         </button>
       </div>
     </form>
