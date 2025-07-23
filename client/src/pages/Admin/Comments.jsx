3e943690d731db013af9f98ed7b731d90ef7c85e
Nebiyu Daniel <danielnebiyuneb@gmail.com>
2025-07-23 12:57:19 +0300

    Comments

--- client/src/pages/Admin/Comments.jsx
+++ client/src/pages/Admin/Comments.jsx
@@ -1,13 +1,23 @@
 import React, { useEffect, useState } from "react";
 import { comments_data } from "../../assets/QuickBlog-Assets/assets";
 import CommentTableItems from "../../components/Admin/CommentTableItems";
+import { useAppContext } from "../../Context/AppContext";
+import toast from "react-hot-toast";
 
 function Comments() {
+  const { data } = useAppContext();
+
   const [comments, setComments] = useState([]);
   const [filter, setFilter] = useState("Not Approved");
 
   const fetchComments = async () => {
-    setComments(comments_data);
+    try {
+      const { data } = await axios.get("/api/admin/comments");
+      data.success ? setComments(data.comments) : toast.error(data.message);
+    } catch (error) {
+      console.log("Error fetching comments:", error);
+      toast.error("Failed to fetch comments");
+    }
   };
 
   useEffect(() => {
@@ -18,45 +28,58 @@ function Comments() {
       <div className="flex justify-between items-center max-w-3xl">
         <h1>Comments</h1>
         <div className="flex gap-4">
-          <button onClick={()=> setFilter('Approved')}
-            className={`shadow-custom-sm border rounded-full px-4 py-1 cursor-pointer text-xs ${filter === 'Approved' ? 'text-primary' : 'text-gray-700'}`}
+          <button
+            onClick={() => setFilter("Approved")}
+            className={`shadow-custom-sm border rounded-full px-4 py-1 cursor-pointer text-xs ${
+              filter === "Approved" ? "text-primary" : "text-gray-700"
+            }`}
           >
             Approved
           </button>
 
-          <button  onClick={()=> setFilter('Not Approved')}
-            className={`shadow-custom-sm border rounded-full px-4 py-1 cursor-pointer text-xs ${filter === 'Not Approved' ? 'text-primary' : 'text-gray-700'}`}
+          <button
+            onClick={() => setFilter("Not Approved")}
+            className={`shadow-custom-sm border rounded-full px-4 py-1 cursor-pointer text-xs ${
+              filter === "Not Approved" ? "text-primary" : "text-gray-700"
+            }`}
           >
             Not Approved
           </button>
         </div>
-      </div> 
+      </div>
 
       <div className="relative h-4/5 max-w-3xl overflow-x-auto mt-4 bg-white shadow rounded-lg scrollbar-hide">
-      <table className="w-full text-sm text-gray-500">
-        <thead className="text-sm text-gray-700 text-left uppercase">
-          <tr>
-            <th scope="col" className="px-6 py-3">
-              Blog Title & Comment
-            </th>
-            <th scope="col" className="px-6 py-3 max-sm:hidden">
-              Date
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Action
-            </th>
-          </tr>
-        </thead> 
+        <table className="w-full text-sm text-gray-500">
+          <thead className="text-sm text-gray-700 text-left uppercase">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                Blog Title & Comment
+              </th>
+              <th scope="col" className="px-6 py-3 max-sm:hidden">
+                Date
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Action
+              </th>
+            </tr>
+          </thead>
 
-        <tbody>
-          {comments_data.filter((comment)=>{
-            if (filter === 'Approved') return comment.isApproved === true;
-            return comment.isApproved === false;
-          }).map((comment,index)=>(
-            <CommentTableItems key={comment._id} comment={comment} index={index + 1} fetchComments={fetchComments}/>
-          ))}
-        </tbody>
-      </table>
+          <tbody>
+            {comments
+              .filter((comment) => {
+                if (filter === "Approved") return comment.isApproved === true;
+                return comment.isApproved === false;
+              })
+              .map((comment, index) => (
+                <CommentTableItems
+                  key={comment._id}
+                  comment={comment}
+                  index={index + 1}
+                  fetchComments={fetchComments}
+                />
+              ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
